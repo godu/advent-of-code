cf6be256d96eb031bdd6c4674bb65db7c580813f
godu <arthur.weber@viacesi.fr>
2018-12-15 15:41:49 +0100

    Reworks day1/2

--- 2018/ocaml/lib/day_1.ml
+++ 2018/ocaml/lib/day_1.ml
@@ -1,22 +1,22 @@
-let fold (initialValue: int) (frequencies: int list): int =
-  List.fold_left (+) initialValue frequencies
-let%test _ = 
-  [ 1; 1; 1 ]
-  |> fold 0
-  |> (=) 3
-let%test _ = 
-  [ 1; 1; -2 ]
-  |> fold 0
-  |> (=) 0
-let%test _ = 
-  [ -1; -2; -3 ]
-  |> fold 0
-  |> (=) (-6)
+open Core_kernel
 
-module Int_set =  Set.Make(
-  struct let compare = Pervasives.compare 
-    type t = int
-  end)
+let fold (initialValue: int) (frequencies: int list): int =
+  List.fold_left ~init:initialValue ~f:(+) frequencies
+let%test_unit _ =
+  [%test_eq: int] (
+    [ 1; 1; 1 ]
+    |> fold 0
+  ) 3;
+  [%test_eq: int] (
+    [ 1; 1; -2 ]
+    |> fold 0
+  ) 0;
+  [%test_eq: int] (
+    [ -1; -2; -3 ]
+    |> fold 0
+  ) (-6);
+
+module Int_set =  Set.Make(Int)
 
 let part_1 (frequencies: int list) =
   frequencies
@@ -25,26 +25,37 @@ let%test _ = [ 1; 1; 1; 1; 1; -2; -1; -2; -3 ] |> part_1 |> (=) (-3)
 
 let part_2 (frequencies: int list) =
   frequencies
-  |> Base.Sequence.repeat
-  |> Base.Sequence.concat_map ~f: Base.Sequence.of_list
-  |> Base.Sequence.append (Base.Sequence.of_list [0])
-  |>  Base.Sequence.folding_map
+  |> Sequence.repeat
+  |> Sequence.concat_map ~f: Sequence.of_list
+  |> Sequence.append (Sequence.of_list [0])
+  |>  Sequence.folding_map
     ~init: 0
     ~f: (fun acc current -> 
         let next = (+) acc current in
         (next, next)
       )
-  |> Base.Sequence.fold_until
-    ~init: Int_set.empty
-    ~f: (fun set value -> 
-        if Int_set.mem value set
-        then
-          Base.Continue_or_stop.Stop(value)
-        else 
-          Base.Continue_or_stop.Continue(Int_set.add value set)
+  |> Sequence.fold_until
+    ~init:Int_set.empty
+    ~f:(fun set value ->
+        match Int_set.mem set value with
+        | true -> Base.Continue_or_stop.Stop(value)
+        | false -> Base.Continue_or_stop.Continue(Int_set.add set value)
       )
     ~finish: (fun _ -> 0)
-let%test _ = [ 1; (-1) ] |> part_2 |> (=) 0
-let%test _ = [ 3; 3; 4; (-2); (-4) ] |> part_2 |> (=) 10
-let%test _ = [ (-6); 3; 8; 5; (-6) ] |> part_2 |> (=) 5
-let%test _ = [ 7; 7; (-2); (-7); (-4) ] |> part_2 |> (=) 14
+let%test_unit _ =
+  [%test_eq: int] (
+    [ 1; (-1) ]
+    |> part_2
+  ) 0;
+  [%test_eq: int] (
+    [ 3; 3; 4; (-2); (-4) ]
+    |> part_2
+  ) 10;
+  [%test_eq: int] (
+    [ (-6); 3; 8; 5; (-6) ]
+    |> part_2
+  ) 5;
+  [%test_eq: int] (
+    [ 7; 7; (-2); (-7); (-4) ]
+    |> part_2
+  ) 14;
--- 2018/ocaml/lib/day_2.ml
+++ 2018/ocaml/lib/day_2.ml
@@ -1,39 +1,62 @@
+open Core_kernel
 open Util
 
 module String_map = Map.Make(String)
 
 let process_word (word: string) =
   word
-  |> String.to_seq
-  |> Seq.fold_left
-    (fun map char -> 
-       String_map.update
-         (String.make 1 char)
-         (fun (v) -> 
-            match v with
-            | Some count -> Some (count + 1)
-            | None -> Some 1
-         )
-         map
-    )
-    String_map.empty
+  |> String.to_list
+  |> Sequence.of_list
+  |> Sequence.fold
+    ~init:String_map.empty
+    ~f:(fun map char ->
+        String_map.update
+          ~f:(fun (v) ->
+              match v with
+              | Some count -> count + 1
+              | None -> 1
+            )
+          map
+          (String.make 1 char)
+      )
   |> (fun map -> 
-      let hasExactlyTwo = String_map.exists (fun _ count -> count = 2) map  in
-      let hasExactlyThree = String_map.exists (fun _ count -> count = 3) map in
+      let hasExactlyTwo = map |> String_map.exists ~f:(fun count -> count = 2)   in
+      let hasExactlyThree = map |> String_map.exists ~f:(fun count -> count = 3) in
 
       ( 
         hasExactlyTwo,
         hasExactlyThree
       )
     )
-
-let%test _ = process_word "abcdef" = (false, false)
-let%test _ = process_word "bababc" = (true, true)
-let%test _ = process_word "abbcde" = (true, false)
-let%test _ = process_word "abcccd" = (false, true)
-let%test _ = process_word "aabcdd" = (true, false)
-let%test _ = process_word "abcdee" = (true, false)
-let%test _ = process_word "ababab" = (false, true)
+let%test_unit _ =
+  [%test_eq: bool * bool] (
+    "abcdef"
+    |> process_word
+  ) (false, false);
+  [%test_eq: bool * bool] (
+    "bababc"
+    |> process_word
+  ) (true, true);
+  [%test_eq: bool * bool] (
+    "abbcde"
+    |> process_word
+  ) (true, false);
+  [%test_eq: bool * bool] (
+    "abcccd"
+    |> process_word
+  ) (false, true);
+  [%test_eq: bool * bool] (
+    "aabcdd"
+    |> process_word
+  ) (true, false);
+  [%test_eq: bool * bool] (
+    "abcdee"
+    |> process_word
+  ) (true, false);
+  [%test_eq: bool * bool] (
+    "ababab"
+    |> process_word
+  ) (false, true)
 
 let int_of_bool bool =
   match bool with
@@ -42,66 +65,88 @@ let int_of_bool bool =
 
 let process_words (words: string list) =
   words
-  |> List.map process_word
-  |> List.fold_left
-    (fun (hasExactlyTwoCount, hasExactlyThreeCount) (hasExactlyTwo, hasExactlyThree) -> 
-       (
-         (hasExactlyTwo |> int_of_bool |> (+) hasExactlyTwoCount), 
-         (hasExactlyThree |> int_of_bool |> (+) hasExactlyThreeCount)
-       )
-    )
-    (0, 0)
+  |> List.map ~f:process_word
+  |> List.fold
+    ~init:(0, 0)
+    ~f:(fun (hasExactlyTwoCount, hasExactlyThreeCount) (hasExactlyTwo, hasExactlyThree) ->
+        (
+          (hasExactlyTwo |> int_of_bool |> (+) hasExactlyTwoCount),
+          (hasExactlyThree |> int_of_bool |> (+) hasExactlyThreeCount)
+        )
+      )
   |> fun (hasExactlyTwoCount, hasExactlyThreeCount) -> hasExactlyTwoCount * hasExactlyThreeCount
-let%test _ = process_words [ "abcdef"; "bababc"; "abbcde"; "abcccd"; "aabcdd"; "abcdee"; "ababab" ] = 12
+let%test_unit _ =
+  [%test_eq: int] (
+    [ "abcdef"; "bababc"; "abbcde"; "abcccd"; "aabcdd"; "abcdee"; "ababab" ]
+    |> process_words
+  ) 12
 
 let part_1 (words: string list): int =
   words
   |> process_words
 
-let list_of_string = String.to_seq >> List.of_seq >> List.map (String.make 1)
+let list_of_string = String.to_list >> List.map ~f:(String.make 1)
 
 let compare_two_words a b =
   (list_of_string b) 
-  |> List.map2 (fun a b -> 
+  |> List.map2_exn ~f:(fun a b ->
       if a = b
       then (0, a)
       else (1, "")
     ) (list_of_string a)
-  |> List.fold_left
-    (fun (countA, charA) (countB, charB) -> 
-       (countA + countB, charA ^ charB)
-    )
-    (0, "")
-let%test _ = compare_two_words "abcde" "axcye" = (2, "ace")
-let%test _ = compare_two_words "fghij" "fguij" = (1, "fgij")
+  |> List.fold
+    ~init:(0, "")
+    ~f:(fun (countA, charA) (countB, charB) ->
+        (countA + countB, charA ^ charB)
+      )
+let%test_unit _ =
+  [%test_eq: int * string] (
+    compare_two_words "abcde" "axcye"
+  ) (2, "ace");
+  [%test_eq: int * string] (
+    compare_two_words "fghij" "fguij"
+  ) (1, "fgij")
 
 let compare_each_words (words: string list) =
-  Base.Sequence.unfold
+  Sequence.unfold
     ~init: words
     ~f: (fun state -> 
         match state with
-        | current_word :: next_state -> Some (
-            List.map (compare_two_words current_word) next_state,
+        | current_word :: next_state ->
+          Some (
+            next_state |> List.map ~f:(compare_two_words current_word),
             next_state
           )
         | _ -> None
       )
-  |> Base.Sequence.to_list
-  |> List.flatten
-let%test _ = compare_each_words ["fghij"; "fguij"] = [(1, "fgij")]
-let%test _ = compare_each_words ["abcde"; "fghij"; "fguij"] = [(5, ""); (5, ""); (1, "fgij")]
+  |> Sequence.to_list
+  |> List.concat
+let%test_unit _ =
+  [%test_eq: (int * string) list] (
+    ["fghij"; "fguij"]
+    |> compare_each_words
+  ) [(1, "fgij")];
+  [%test_eq: (int * string) list] (
+    ["abcde"; "fghij"; "fguij"]
+    |> compare_each_words
+  ) [(5, ""); (5, ""); (1, "fgij")]
 
 let take_most_closer (diffs: (int * string) list): string = 
   diffs
-  |> List.fold_left
-    (fun (diffCountA, commonA) (diffCountB, commonB) -> 
-       if diffCountA > diffCountB
-       then (diffCountB, commonB)
-       else (diffCountA, commonA)
-    )
-    (max_int, "")
+  |> List.fold
+    ~init:(Int.max_value, "")
+    ~f:(fun (diffCountA, commonA) (diffCountB, commonB) ->
+        if diffCountA > diffCountB
+        then (diffCountB, commonB)
+        else (diffCountA, commonA)
+      )
   |> snd
 let%test _ = take_most_closer [(5, ""); (5, ""); (1, "fgij")] = "fgij"
+let%test_unit _ =
+  [%test_eq: string] (
+    [(5, ""); (5, ""); (1, "fgij")]
+    |> take_most_closer
+  ) "fgij"
 
 let part_2 (words: string list) =
   words
