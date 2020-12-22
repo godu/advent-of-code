5d1f7a64f95b2e726e351ee3daaf5295827423e0
godu <arthur.weber@viacesi.fr>
2020-12-22 20:15:58 +0100

    Day22#2

--- src/AdventOfCode/Day22.hs
+++ src/AdventOfCode/Day22.hs
@@ -4,22 +4,25 @@ module AdventOfCode.Day22
   )
 where
 
+import Data.List (find, unfoldr)
+import qualified Data.Set as S (Set, empty, insert)
+import Debug.Trace (traceShowId)
 import Text.ParserCombinators.ReadP (eof, sepBy, skipSpaces, string)
-import Text.Read (Read (readPrec), lift, minPrec, readPrec_to_P)
+import Text.Read (Read (readPrec), ReadPrec, lift, minPrec, readPrec_to_P)
 
 type Card = Int
 
-data Player = Player Int [Card] deriving (Show)
+newtype Player = Player [Card] deriving (Show, Ord, Eq)
 
 instance Read Player where
   readPrec = do
     lift $ string "Player "
-    id <- readPrec
+    (readPrec :: ReadPrec Int)
     lift $ string ":\n"
     cards <- lift $ sepBy (readPrec_to_P readPrec minPrec) (string "\n")
-    return $ Player id cards
+    return $ Player cards
 
-data Game = Game Player Player deriving (Show)
+data Game = Game Player Player deriving (Show, Ord, Eq)
 
 instance Read Game where
   readPrec = do
@@ -30,22 +33,76 @@ instance Read Game where
     lift eof
     return $ Game playerA playerB
 
+deck :: Game -> [Card]
+deck (Game (Player []) (Player bs)) = bs
+deck (Game (Player as) _) = as
+
 answer :: [Card] -> Int
 answer = sum . fmap (uncurry (*)) . zip [1 ..] . reverse
 
 process1 :: Game -> Int
-process1 (Game (Player _ []) (Player _ bs)) = answer bs
-process1 (Game (Player _ as) (Player _ [])) = answer as
-process1 (Game (Player idA (a : as)) (Player idB (b : bs))) =
-  if a <= b
-    then process1 (Game (Player idA as) (Player idB (bs <> [b, a])))
-    else process1 (Game (Player idA (as <> [a, b])) (Player idB bs))
+process1 =
+  maybe 0 (answer . deck)
+    . find isEnd
+    . rounds
+  where
+    isEnd :: Game -> Bool
+    isEnd (Game (Player []) _) = True
+    isEnd (Game _ (Player [])) = True
+    isEnd _ = False
+
+    rounds :: Game -> [Game]
+    rounds = unfoldr go
+      where
+        go :: Game -> Maybe (Game, Game)
+        go (Game (Player []) (Player bs)) = Nothing
+        go (Game (Player as) (Player [])) = Nothing
+        go (Game (Player (a : as)) (Player (b : bs))) =
+          return $
+            (\g -> (g, g)) $
+              if a >= b
+                then Game (Player (as <> [a, b])) (Player bs)
+                else Game (Player as) (Player (bs <> [b, a]))
 
 run1 :: String -> String
 run1 = show . process1 . read
 
-process2 :: Int -> Int
-process2 = id
+process2 :: Game -> Int
+process2 = answer . deck . last . rounds
+  where
+    rounds :: Game -> [Game]
+    rounds = takeUntilDoesntOccured . unfoldr go
+      where
+        takeUntilDoesntOccured :: Ord a => [a] -> [a]
+        takeUntilDoesntOccured = go S.empty
+          where
+            go :: Ord a => S.Set a -> [a] -> [a]
+            go _ [] = []
+            go s (x : xs) =
+              if x `elem` s
+                then []
+                else x : go (x `S.insert` s) xs
+        -- go a
+
+        go :: Game -> Maybe (Game, Game)
+        go (Game (Player []) (Player bs)) = Nothing
+        go (Game (Player as) (Player [])) = Nothing
+        go (Game (Player (a : as)) (Player (b : bs)))
+          | a <= length as && b <= length bs =
+            return $
+              (\g -> (g, g)) $
+                if not . playerBWin $ last $ rounds (Game (Player $ take a as) (Player $ take b bs))
+                  then Game (Player (as <> [a, b])) (Player bs)
+                  else Game (Player as) (Player (bs <> [b, a]))
+          | otherwise =
+            return $
+              (\g -> (g, g)) $
+                if a >= b
+                  then Game (Player (as <> [a, b])) (Player bs)
+                  else Game (Player as) (Player (bs <> [b, a]))
+          where
+            playerBWin :: Game -> Bool
+            playerBWin (Game (Player as) _) = null as
 
 run2 :: String -> String
-run2 = const ""
+run2 = show . process2 . read
--- test/AdventOfCode/Day22Spec.hs
+++ test/AdventOfCode/Day22Spec.hs
@@ -32,4 +32,28 @@ spec = do
       \10\n"
       `shouldBe` "306"
   it "run2" $ do
-    run2 "" `shouldBe` ""
+    run2
+      "Player 1:\n\
+      \9\n\
+      \2\n\
+      \6\n\
+      \3\n\
+      \1\n\
+      \\n\
+      \Player 2:\n\
+      \5\n\
+      \8\n\
+      \4\n\
+      \7\n\
+      \10\n"
+      `shouldBe` "291"
+    run2
+      "Player 1:\n\
+      \43\n\
+      \19\n\
+      \\n\
+      \Player 2:\n\
+      \2\n\
+      \29\n\
+      \14\n"
+      `shouldBe` "105"
